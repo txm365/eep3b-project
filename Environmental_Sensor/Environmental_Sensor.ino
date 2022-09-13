ef60a14b4174f268cf1ea8877da8a5c4689dd6ad
txm365 <thabang.moliane@outlook.com>
2022-09-14 01:52:29 +0200

    Fixed LCD Screen

--- Environmental_Sensor/Environmental_Sensor.ino
+++ Environmental_Sensor/Environmental_Sensor.ino
@@ -1,9 +1,17 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(27,26,33,32,19,23);
+void setup()
+{
+ 
+    lcd.begin(16, 2);
+    lcd.clear();
+    lcd.print("How to Interface");
+    
 
+    // go to row 1 column 0, note that this is indexed at 0
+    lcd.setCursor(0,1); 
+    lcd.print ("LCD with ESP32");
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+void loop(){
+  
+  }
