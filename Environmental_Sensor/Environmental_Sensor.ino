5f401430c48f24b11b8921138560a4f1fd881c6d
txm365 <thabang.moliane@outlook.com>
2022-09-21 19:04:22 +0200

    Dynamic Wifi Credentials. LCD Formatting

--- Environmental_Sensor/Environmental_Sensor.ino
+++ Environmental_Sensor/Environmental_Sensor.ino
@@ -1,21 +1,23 @@
 #include <LiquidCrystal.h>
 #include <WiFi.h>
+#include <WiFiManager.h>
 #include "time.h"
 
 LiquidCrystal lcd(27,26,33,32,19,23);           // Assign lcd pins
 
 char line1[21],line2[21],line3[21],line4[21];  //Initialize line data buffers
-
-//set network parameters
-const char* ssid       = "BrightMindsBoyz[2GHz]";
-const char* password   = "D@phn313B0yz";
+char local_net_ssid[21];                       //Network Parameters
 
 //Set time parameters
 int tmz = 1;           // Set timezone
 const char* ntpServer = "pool.ntp.org";
 long  gmtOffset_sec = tmz*3600;            //timezone setting
 const int   daylightOffset_sec = 3600;
+bool res;
 
+String myssid;
+String myIP;
+String myRSSI;
 void setup()
 {
     //Format the LCD buffers with blank spaces
@@ -24,6 +26,8 @@ void setup()
     sprintf(line3,"%s","                    ");
     sprintf(line4,"%s","                    ");
 
+    sprintf(local_net_ssid,"%s","                    "); 
+     
     lcd.begin(20, 4);   //Initialize screen as a 20x4
     lcd.clear();
     //clear off the lcd screen spaces
@@ -36,19 +40,56 @@ void setup()
     lcd.setCursor(3,1); 
     lcd.print(line4);
 
+ 
     //connect to WiFi
-    lcd.setCursor(0,1);
-    lcd.print(" Connecting to WIFI ");
-    WiFi.begin(ssid, password);
+     
+      lcd.setCursor(0,1);
+      lcd.print("Connecting to  WIFI ");
+      WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+      WiFiManager wm; 
     while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-  }
-  lcd.setCursor(0,1);
-  lcd.print("     CONNECTED      ");
-  delay(5000);
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  printLocalTime();
+      
+//      delay(2000);
+        lcd.setCursor(0,0);
+        lcd.print("previously connected");
+        lcd.setCursor(0,1);
+        lcd.print("wifi connot be found");
+        lcd.setCursor(0,2);
+        lcd.print("Change wifi details:");
+        lcd.setCursor(0,3);
+        lcd.print(" http://192.168.4.1 "); 
+       
+        res = wm.autoConnect("ESP_WIFI_SETUP","setup_password"); // password protected ap
+    
+        if(!res) {
+            Serial.println("Failed to connect");
+            ESP.restart();
+         }
+         else{
+          delay(2000);
+
+       
+        
+        lcd.setCursor(0,0);
+        lcd.print("    Connected to    ");
+        lcd.setCursor(0,1);
+         sprintf(local_net_ssid,"WIFI: %-14s",WiFi.SSID());
+         lcd.print(local_net_ssid);
+         
+         lcd.setCursor(2,2);
+         lcd.print("                    ");
+        lcd.setCursor(2,2);
+        lcd.print(WiFi.localIP());
+        
+        lcd.setCursor(0,3);
+        lcd.print("                    ");
+            delay(5000);
+            configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+            printLocalTime();
+         }
+     } 
 }
+
 void loop(){
   delay(1000);
   printLocalTime();
@@ -57,7 +98,7 @@ void loop(){
 void printLocalTime(){
   struct tm timeinfo;
   if(!getLocalTime(&timeinfo)){
-    
+    lcd.clear();
    sprintf(line2,"%s","Time Update Failed! ");
    lcd.setCursor(0,1);
    lcd.print(line2);
@@ -68,7 +109,9 @@ void printLocalTime(){
   lcd.setCursor(0,1);
   lcd.print(&timeinfo, "      %A        ");
   lcd.setCursor(0,2);
-  lcd.print(&timeinfo, " %B %d %Y");
+  lcd.print(&timeinfo, "  %d %B %Y ");
+  lcd.setCursor(0,3);
+  lcd.print(&timeinfo, "                    ");
  
   
 }
