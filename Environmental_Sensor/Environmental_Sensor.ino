ba3c130c203d9d7209e76aaa1f986ae857ef8879
txm365 <thabang.moliane@outlook.com>
2022-09-28 23:58:27 +0200

    MQTT Connection Established - TX/RX

--- Environmental_Sensor/Environmental_Sensor.ino
+++ Environmental_Sensor/Environmental_Sensor.ino
@@ -2,6 +2,7 @@
 #include <WiFi.h>
 #include <WiFiManager.h>
 #include "time.h"
+#include <PubSubClient.h>
 
 #define RS 27
 #define EN 26
@@ -13,6 +14,7 @@
 LiquidCrystal lcd(RS,EN,D4,D5,D6,D7);           // Assign lcd pins
 
 char line1[21],line2[21],line3[21],line4[21];  //Initialize line data buffers
+char times[9];
 
 //Set time parameters
 int tmz = 1;           // Set timezone
@@ -21,9 +23,21 @@ long  gmtOffset_sec = tmz*3600;            //timezone setting
 const int   daylightOffset_sec = 3600;
 bool res;
 bool timeConfigured = false;
+unsigned long lasttick= 0;
+
+// MQTT Broker
+const char *mqtt_broker = "broker.emqx.io";
+const char *topic = "txm365/esp32/temp";
+const char *mqtt_username = "emqx";
+const char *mqtt_password = "public";
+const int mqtt_port = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
     //Format the LCD buffers with blank spaces
     sprintf(line1,"%s","                    ");
     sprintf(line2,"%s","                    ");
@@ -68,35 +82,65 @@ void setup()
             Serial.println("Failed to connect");
             lcd.setCursor(0,1);
             lcd.print("  Failed to connect ");
-            delay(3000);
+            delay(15000);
             ESP.restart();
         }  
       
    }
-     
-         Serial.println("Connected");
          
         lcd.setCursor(0,0);
         lcd.print("    Connected to    ");
         lcd.setCursor(0,1);
         lcd.print("                    ");
-         lcd.print(WiFi.SSID());
-         lcd.setCursor(0,2);
-         lcd.print(" Network IP ADDRESS ");
-         lcd.setCursor(0,3);
-         lcd.print("                    ");
+        lcd.setCursor(0,1);
+        lcd.print(WiFi.SSID());
+        lcd.setCursor(0,2);
+        lcd.print(" Network IP ADDRESS ");
+        lcd.setCursor(0,3);
+        lcd.print("                    ");
         lcd.setCursor(2,3);
         lcd.print(WiFi.localIP());
         
-         delay(10000);
+        delay(10000);
+        
          configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
          timeConfigured = true;
-     
-        
+
+         //connecting to a mqtt broker
+       client.setServer(mqtt_broker, mqtt_port);
+       client.setCallback(callback);
+       while (!client.connected()) {
+           String client_id = "esp32-client-txm365";
+           client_id += String(WiFi.macAddress());
+           Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
+           if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+               Serial.println("Public emqx mqtt broker connected");
+           } else {
+               Serial.print("failed with state ");
+               Serial.print(client.state());
+               delay(2000);
+           }
+       }
+       // publish and subscribe
+       client.publish(topic, "Hi EMQX I'm ESP32 ^^");
+       client.subscribe(topic);
+      }
+      
+      void callback(char *topic, byte *payload, unsigned int length) {
+       Serial.print("Message arrived in topic: ");
+       Serial.println(topic);
+       Serial.print("Message:");
+       for (int i = 0; i < length; i++) {
+           Serial.print((char) payload[i]);
+       }
+         
 }
 
 void loop(){
-  delay(1000);
+  client.loop();
+  
+  if (millis() - lasttick > 1000){
+    lasttick = millis();
     if(timeConfigured){
       printLocalTime();
     }
@@ -111,6 +155,7 @@ void loop(){
          lcd.print(" updated Server time ");
          lcd.setCursor(0,3);
          lcd.print("                    ");  
+     }
   }
 }
 
@@ -130,7 +175,9 @@ void printLocalTime(){
   lcd.setCursor(0,2);
   lcd.print(&timeinfo, "  %d %B %Y ");
   lcd.setCursor(0,3);
-  lcd.print(&timeinfo, "                    ");
+  lcd.print("                    ");
  Serial.println(&timeinfo, "      %H:%M:%S      ");//
+ sprintf(times,"%02d:%02d:%02d",timeinfo.tm_hour,timeinfo.tm_min, timeinfo.tm_sec);
+ client.publish(topic, times);
   
 }
