33e3535142cda02b7ee03ba4f0934b789a691251
txm365 <thabang.moliane@outlook.com>
2022-09-28 19:57:11 +0200

    Fixed bugs

--- Environmental_Sensor/Environmental_Sensor.ino
+++ Environmental_Sensor/Environmental_Sensor.ino
@@ -3,10 +3,16 @@
 #include <WiFiManager.h>
 #include "time.h"
 
-LiquidCrystal lcd(27,26,33,32,19,23);           // Assign lcd pins
+#define RS 27
+#define EN 26
+#define D4 33
+#define D5 32
+#define D6 19
+#define D7 25
+
+LiquidCrystal lcd(RS,EN,D4,D5,D6,D7);           // Assign lcd pins
 
 char line1[21],line2[21],line3[21],line4[21];  //Initialize line data buffers
-char local_net_ssid[21];                       //Network Parameters
 
 //Set time parameters
 int tmz = 1;           // Set timezone
@@ -14,19 +20,15 @@ const char* ntpServer = "pool.ntp.org";
 long  gmtOffset_sec = tmz*3600;            //timezone setting
 const int   daylightOffset_sec = 3600;
 bool res;
-
-String myssid;
-String myIP;
-String myRSSI;
+bool timeConfigured = false;
 void setup()
 {
+  Serial.begin(115200);
     //Format the LCD buffers with blank spaces
     sprintf(line1,"%s","                    ");
     sprintf(line2,"%s","                    ");
     sprintf(line3,"%s","                    ");
     sprintf(line4,"%s","                    ");
-
-    sprintf(local_net_ssid,"%s","                    "); 
      
     lcd.begin(20, 4);   //Initialize screen as a 20x4
     lcd.clear();
@@ -40,60 +42,77 @@ void setup()
     lcd.setCursor(3,1); 
     lcd.print(line4);
 
- 
-    //connect to WiFi
-     
       lcd.setCursor(0,1);
-      lcd.print("Connecting to  WIFI ");
+      lcd.print(" Connecting to WIFI ");
+      delay(2000);
+    //connect to WiFi
+    
+ 
+    while ((WiFi.status() != WL_CONNECTED)){
       WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
-      WiFiManager wm; 
-    while (WiFi.status() != WL_CONNECTED) {
+      WiFiManager wm;
       
-//      delay(2000);
+   
         lcd.setCursor(0,0);
-        lcd.print("previously connected");
+        lcd.print("  Connect to wifi:  ");
         lcd.setCursor(0,1);
-        lcd.print("wifi connot be found");
+        lcd.print("   ESP_WIFI_SETUP   ");
         lcd.setCursor(0,2);
-        lcd.print("Change wifi details:");
+        lcd.print("On your phone, then ");
         lcd.setCursor(0,3);
         lcd.print(" http://192.168.4.1 "); 
-       
-        res = wm.autoConnect("ESP_WIFI_SETUP","setup_password"); // password protected ap
-    
-        if(!res) {
-            Serial.println("Failed to connect");
-            ESP.restart();
-         }
-         else{
-          delay(2000);
-
-       
         
+      
+      res = wm.autoConnect("ESP_WIFI_SETUP","setup_password"); // password protected ap
+      if(!res) {
+            Serial.println("Failed to connect");
+            lcd.setCursor(0,1);
+            lcd.print("  Failed to connect ");
+            delay(3000);
+            ESP.restart();
+        }  
+      
+   }
+     
+         Serial.println("Connected");
+         
         lcd.setCursor(0,0);
         lcd.print("    Connected to    ");
         lcd.setCursor(0,1);
-         sprintf(local_net_ssid,"WIFI: %-14s",WiFi.SSID());
-         lcd.print(local_net_ssid);
-         
-         lcd.setCursor(2,2);
+        lcd.print("                    ");
+         lcd.print(WiFi.SSID());
+         lcd.setCursor(0,2);
+         lcd.print(" Network IP ADDRESS ");
+         lcd.setCursor(0,3);
          lcd.print("                    ");
-        lcd.setCursor(2,2);
+        lcd.setCursor(2,3);
         lcd.print(WiFi.localIP());
         
-        lcd.setCursor(0,3);
-        lcd.print("                    ");
-            delay(5000);
-            configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-            printLocalTime();
-         }
-     } 
+         delay(10000);
+         configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+         timeConfigured = true;
+     
+        
 }
 
 void loop(){
   delay(1000);
-  printLocalTime();
+    if(timeConfigured){
+      printLocalTime();
+    }
+    else {
+     lcd.setCursor(0,0);
+        lcd.print("                    ");
+        lcd.setCursor(0,1);
+        lcd.print("                    ");
+         lcd.setCursor(0,1);
+         lcd.print("   Running without  ");
+         lcd.setCursor(0,2);
+         lcd.print(" updated Server time ");
+         lcd.setCursor(0,3);
+         lcd.print("                    ");  
   }
+}
 
 void printLocalTime(){
   struct tm timeinfo;
@@ -112,6 +131,6 @@ void printLocalTime(){
   lcd.print(&timeinfo, "  %d %B %Y ");
   lcd.setCursor(0,3);
   lcd.print(&timeinfo, "                    ");
- 
+ Serial.println(&timeinfo, "      %H:%M:%S      ");//
   
 }
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # eep3b-project
 The development process of an EEP3B project 
+*********************************************
+
+Use of ESP32-Wroom Microcontoroller
+*For Wifi capabilities
+*Compatibility with the arduino IDE
